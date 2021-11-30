e5353947df16aadb750877a759657413fe5a3489
krisikostadinov <krisi.199898@gmail.com>
2021-11-30 13:02:22 +0200

    Listing of all animals types and breed of any farm.

--- server/routes/animals.js
+++ server/routes/animals.js
@@ -4,7 +4,7 @@ const { isAuthenticated } = require('../config/restrictions');
 
 const router = express.Router();
 
-router.post('/create', isAuthenticated, async (req, res, next) => {
+router.post('/create', isAuthenticated, async (req, res) => {
     const { type, count, breed } = { ...req.body };
 
     const animal = await Animal.create({ type, count, farm: res.locals.farmModel._id, breed });
@@ -15,6 +15,22 @@ router.get('/all', isAuthenticated, async (req, res) => {
     const farmId = res.locals.farmModel._id;
     const allAnimals = await Animal.find({ farm: farmId });
     res.send(allAnimals);
+});
+
+router.get('/all/type/:type', isAuthenticated, async (req, res) => {
+    const type = req.params.type;
+    const farmId = res.locals.farmModel._id;
+
+    const allOfTypeAnimals = await Animal.find({ farm: farmId, type });
+    res.send(allOfTypeAnimals);
+});
+
+router.get('/all/breed/:breed', isAuthenticated, async (req, res) => {
+    const breed = req.params.breed;
+    const farmId = res.locals.farmModel._id;
+
+    const allOfBreedAnimals = await Animal.find({ farm: farmId, breed });
+    res.send(allOfBreedAnimals);
 });
 
 module.exports = router;
