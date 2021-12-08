a5b4703dda0f0339f5df6cd4f8d215b6af7aa9e6
krisikostadinov <krisi.199898@gmail.com>
2021-12-08 13:20:31 +0200

    heroku

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,7 @@
 module.exports = {
-    db: 'mongodb://127.0.0.1/farm',
+    db: 'mongodb+srv://admin:0882974259@cluster0.zh8iu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     secret_jwt: 'jdxjkdlllfldll'
 }
 
 //mongodb+srv://admin:0882974259@cluster0.zh8iu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//mongodb://127.0.0.1/farm
