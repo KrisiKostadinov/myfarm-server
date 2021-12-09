1b3a2bc682edf8d291cf2ff0490daf0f1be934ef
krisikostadinov <krisi.199898@gmail.com>
2021-12-09 12:16:01 +0200

    Make it better

--- config/configuration.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    db: 'mongodb+srv://admin:0882974259@cluster0.zh8iu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    secret_jwt: 'jdxjkdlllfldll'
-}
-
-//mongodb+srv://admin:0882974259@cluster0.zh8iu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-//mongodb://127.0.0.1/farm
--- config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('../config/configuration');
-
-mongoose.connect(config.db, () => {
-    console.log('Database is ready!');
-});
